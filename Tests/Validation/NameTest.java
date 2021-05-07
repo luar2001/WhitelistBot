d63c1dabd272d0f540f623a52bb0665fc6c334ac
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-07 21:40:30 +0200

    Started on tests for the username validation

---
+++ Tests/Validation/NameTest.java
@@ -0,0 +1,16 @@
+package Validation;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 07/05/2021
+ * Time: 21:33
+ * Project: WhitelistBot
+ * Copyright: MIT
+ **/
+public class NameTest {
+
+
+
+
+
+}
